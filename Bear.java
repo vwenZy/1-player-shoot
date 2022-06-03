0672c97da6c6618593c54b2c2793a3676850559d
vwenZy <101139350+vwenZy@users.noreply.github.com>
2022-06-03 14:17:04 -0400

    Add files via upload

--- Bear.java
+++ Bear.java
@@ -8,6 +8,48 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bear extends Actor
 {
+    public static int speed = 3;
+     
+    // GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] shoot = new GreenfootImage[4];
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    /**
+     * Constructor - The code that gets run one time when object is created.
+     */
+    public Bear()
+    {
+        for(int i = 0; i < shoot.length; i++)
+        {
+            shoot[i] = new GreenfootImage("images/Wombat/shoot" + i + ".png");
+            shoot[i].scale(600, 100);
+        }
+        
+        animationTimer.mark();
+        
+        // Initial elephant image.
+        setImage(shoot[0]);
+    }
+    
+    /**
+     * Animate the Bear
+     */
+    int imageIndex = 1;
+    public void animateBear()
+    {
+        if(animationTimer.millisElapsed() < 100) {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right")) {
+            setImage(shoot[imageIndex]);
+            imageIndex = (imageIndex + 1) % shoot.length;
+        } else {
+            setImage(shoot[imageIndex]);
+            imageIndex = (imageIndex + 1) % shoot.length;
+        }
+    }
+    
     /**
      * Act - do whatever the Bear wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -18,11 +60,14 @@ public class Bear extends Actor
         if(MyWorld.shootTime.millisElapsed() > 250 && (Greenfoot.isKeyDown("space")))
         {
             getWorld().addObject(new Bullet(), getX(), getY());
+            animateBear();
             MyWorld.shootTime.mark();
         }
         
         // moving around.
         move();
+        
+        // animateBear();
     }
     
     public void move()
